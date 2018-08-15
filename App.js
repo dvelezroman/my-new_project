a62c171ab3bff0ad4509676195b23af1b2033b48
Dario <dvelezroman@gmail.com>
2018-08-15 15:01:31 -0300

    4to commit

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      numero: 0,
+    }
+  }
+
+  aumentar = () => {
+    this.setState({
+      numero: this.state.numero +1,
+    })
+  }
+
+  disminuir = () => {
+    this.setState({
+      numero: this.state.numero ? this.state.numero -1 : 0,
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.cajaUno}/>
-        <View style={styles.cajaDos}>
-          <Text>Esta es la caja Dos</Text>
-        </View>
-        <View style={styles.cajaTres}/>
-        <View style={styles.cajaCuatro}/>
-      </View>
+      <Grid style={styles.container}>
+        <Row style={styles.cajaTres}><Text>Numero: {this.state.numero}</Text></Row>
+        <Row>
+          <Col style={styles.cajaUno}><Button style={styles.button} title='Aumentar' onPress={() => {this.aumentar()}}/></Col>
+          <Col style={styles.cajaDos}><Button style={styles.button} title='Disminuir' onPress={() => {this.disminuir()}}/></Col>
+        </Row>
+      </Grid>
     );
   }
 }
@@ -24,19 +43,19 @@ const styles = StyleSheet.create({
   cajaUno: {
     flex: 1,
     backgroundColor: '#00ff00',
+    justifyContent: 'flex-start',
+    alignItems: 'center',  
   },
   cajaDos: {
-    flex: 6,
+    flex: 1,
     backgroundColor: '#90ff90',
-    alignItems: 'center',
-    justifyContent: 'center', 
+    justifyContent: 'flex-start',
+    alignItems: 'center', 
   },
   cajaTres: {
-    flex: 15,
-    backgroundColor: '#70ff70',
-  },
-  cajaCuatro: {
     flex: 1,
     backgroundColor: '#50ff50',
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
