897ec34adf996f84886acce9671e10e6959aa8fb
Dario <dvelezroman@gmail.com>
2018-08-15 10:57:16 -0300

    3er commit

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  state = {
-    numero: "11345678",
-  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello world...!{this.state.numero}</Text>
+        <View style={styles.cajaUno}/>
+        <View style={styles.cajaDos}>
+          <Text>Esta es la caja Dos</Text>
+        </View>
+        <View style={styles.cajaTres}/>
+        <View style={styles.cajaCuatro}/>
       </View>
     );
   }
@@ -18,7 +20,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  cajaUno: {
+    flex: 1,
+    backgroundColor: '#00ff00',
+  },
+  cajaDos: {
+    flex: 6,
+    backgroundColor: '#90ff90',
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+  cajaTres: {
+    flex: 15,
+    backgroundColor: '#70ff70',
+  },
+  cajaCuatro: {
+    flex: 1,
+    backgroundColor: '#50ff50',
+  }
 });
